0c4df8d3acc7d7a1907ca65b11e2e93a3e20ae68
Chris <schauer@inf-schauer.de>
2025-08-10 17:36:59 +0200

    logging

--- src/main/kotlin/de/inf_schauer/home_api/controller/HealthController.kt
+++ src/main/kotlin/de/inf_schauer/home_api/controller/HealthController.kt
@@ -1,5 +1,6 @@
 package de.inf_schauer.home_api.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/health")
 class HealthController {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @GetMapping
-    fun health(): String = "healthy"
+    fun health(): String {
+        logger.info("healthy")
+        return "healthy"
+    }
 }
