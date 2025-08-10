6e4a5acc9d10fd99e043bdeb9a2e33eb7bcafc15
Chris <schauer@inf-schauer.de>
2025-08-10 13:07:11 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("plugin.spring") version "1.9.23"
+}
+
+group = "de.inf_schauer"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
